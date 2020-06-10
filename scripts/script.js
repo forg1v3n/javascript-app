0a1fd7b9927ea4f155436dc20bf27f998f9913df
Alisson de Oliveira <aalissonrafael@yahoo.com>
2020-06-09 21:14:02 -0300

    Project completed

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,27 @@ for(var i = 0; i <= 60; i++){
 for(var i = 1; i <= 60; i++){
     seconds.innerHTML+='<option value="'+i+'">'+i+'</option>';
 }
+
+start.addEventListener('click', function(){
+    minutesCurrent = minutes.value;
+    secondsCurrent = seconds.value;
+
+    display.childNodes[1].innerHTML = minutesCurrent + ":"+secondsCurrent;
+
+    interval = setInterval(function(){
+        secondsCurrent--;
+        if(secondsCurrent <= 0) {
+            if(minutesCurrent > 0) {
+                minutesCurrent--;
+                secondsCurrent = 59;
+            }else{
+                alert("Acabou!");
+                document.getElementById("sound").play();
+                clearInterval(interval);
+            }
+        }
+        display.childNodes[1].innerHTML = minutesCurrent + ":"+secondsCurrent;
+        
+
+    },1000);
+})
